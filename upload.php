eb96a835e12849462b8bb25812978d206b57443e
unknown <aymeri@citycollege.sheffield.eu>
2020-04-16 13:39:21 +0200

    New upload.php, not implemented in the website

--- upload.php
+++ upload.php
@@ -1,22 +1,33 @@
 <?php
+        if(isset($_POST['submit'])){
+            $file = $_FILES['file'];
 
-if (($_FILES['userfile']['name']!="")){
-// Where the file is going to be stored
- $target_dir = "C:\xampp\htdocs\images";
- $file = $_FILES['userfile']['name'];
- $path = pathinfo($file);
- $filename = $path['filename'];
- $ext = $path['extension'];
- $temp_name = $_FILES['userfile']['tmp_name'];
- $path_filename_ext = $target_dir.$filename.".".$ext;
- 
-// Check if file already exists
-if (file_exists($path_filename_ext)) {
- echo "Sorry, file already exists.";
- }else{
- if(move_uploaded_file($temp_name,$path_filename_ext));
- echo "Congratulations! File Uploaded Successfully.";
- }
-}
+            $fileName = $_FILES['file']['name'];
+            $fileTmpName = $_FILES['file']['tmp_name'];
+            //Might need to limit size: $fileSize = $_FILES['file']['size'];
+            //Might need for error-checking: $fileError = $_FILES['file']['error'];
+            //IDK $fileType = $_FILES['file']['type'];
+            
+            //Retrieve the part after the '.' (extension)
+            $fileExt = explode('.', $fileName);
+            $fileActualExt = strtolower(end($fileExt));
 
+            //Allowed formats
+            $allowed = array('jpg', 'jpeg', 'bmp', 'png');
+            
+            if(in_array($fileActualExt, $allowed)){
+                    //Instead of test, use get_username from the session.
+                    $fileNameNew = "get_username()".".".$fileActualExt;
+                    $fileDestination = 'images/'.$fileNameNew;
+                    move_uploaded_file($fileTmpName, $fileDestination);
+                    
+                    //Go back to the site that called upload.php
+                    header("location:".$_SERVER['HTTP_REFERER']);
+                    //Testing purposes
+                    //echo '<img src="images/'.$fileNameNew.'">';
+            }
+            else{
+                echo "Inappropriate format";
+            }
+        }
 ?>
