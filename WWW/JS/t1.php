690213b8c1dd4be23e1795d48182125dd1a3770d
unknown <aymeri@citycollege.sheffield.eu>
2020-04-16 09:00:00 +0200

    Added folders

---
+++ WWW/JS/t1.php
@@ -0,0 +1,14 @@
+
+<?php
+
+$firstname = $_POST['memli'];
+$email = $_POST['email'];
+
+
+    echo "NAME: " . $firstname . "<BR>";
+    echo "EMAIL: " . $email . "<BR>";
+
+echo "hello";
+
+?>
+    
---
+++ excercise/003/checkform.js
@@ -0,0 +1,56 @@
+// Edit the function below to work exactly according to the requirement below
+
+
+
+function checkform()
+
+{
+    var m = document.getElementsByName("year").value;
+    alert(m);
+
+
+//  if (date =="")
+//  {
+//      alert('Date is empty');
+//      return true;
+//  }
+
+ 
+//  if (!name)
+//  {
+//      alert('Date is empty');
+//      return true;
+
+//  }
+
+
+//  if(name.split(/\W+/).length > 2){
+//         alert('more than two words');
+//         return true;
+//  }
+
+//  else{
+//      return false
+//  }
+
+
+ 
+
+
+/* You need to check that the value entered for the year field is blank (empty), and if it is, issue an alert
+
+
+
+/* You need to check that the value entered for the fullname field is blank (empty), and if it is, issue an alert
+
+/* You need to check that the value entered for the fullname field contains more than two words, and if it does, issue an alert
+
+if any of the above violated, the function checkform() returns false, otherwise true.
+
+*/
+
+
+}
+
+
+
